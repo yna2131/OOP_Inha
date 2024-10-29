0fc8076de78f53c864fff136b967876d3ce49607
yna2131 <jn7924@gmail.com>
2024-10-29 21:25:05 +0900

    Set basic structure of class Pirate

---
+++ classes/pirate.py
@@ -0,0 +1,15 @@
+class Pirate:
+    def __init__(self, name, weapon, health):
+        self.name = name
+        self.weapon = weapon
+        self.health = health
+        
+    def take_damage(self):
+        pass
+    
+    def attack(self):
+        pass
+    
+# From what I understand, pirates are "other players", like NPCs that you can fight agsint
+# So similar to class activities, we could have methods of taking damage and attacking until one gets defeated
+
