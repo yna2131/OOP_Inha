ea82b41818893d5ec5e017142708c6649837e2de
kama080 <kamronbektokhirov@gmail.com>
2024-11-01 19:17:24 +0900

    Update cargo ship class with inventory class

--- classes/ship.py
+++ classes/ship.py
@@ -16,7 +16,21 @@ class Ship:
         pass
     
 class CargoShip(Ship):
-    pass
+    def __init__(self, max_capacity):
+        self.max_capacity = max_capacity
+        self.current_capacity = 0
+        self.items = {}
+        
+    def add(self):
+        pass
+    
+    def remove(self):
+        pass
+    
+    def display(slef):
+        pass
+    
+# The basic logics realted to managing items; we can add, remove, and display items
 
 class FighterShip(Ship):
     pass
