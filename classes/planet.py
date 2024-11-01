296b50584cb2fe0797802ad75afd19a3d7e65102
yna2131 <jn7924@gmail.com>
2024-11-01 18:41:29 +0900

    Fix typo

--- classes/planet.py
+++ classes/planet.py
@@ -3,7 +3,7 @@ class Planet:
         self.name = name
         self.market_prices = market_prices
         
-    def trading_market(selef):
+    def trading_market(self):
         pass
     
     # Information about the market. We could display the name of the market, and the items and their price
